c099f98c865372fb88af80263f3bef483a257fe6
sourcifylending <sourcifylending@gmail.com>
2026-03-27 15:51:13 -0400

    feat: default to dark mode, brighten gray-500/600 text in dark mode

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -7,7 +7,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="light"
+      defaultTheme="dark"
       enableSystem={false}
       storageKey="sl-theme"
     >
